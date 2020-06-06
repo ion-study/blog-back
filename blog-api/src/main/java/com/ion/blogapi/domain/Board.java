31b1688d8c916996f85b262d08865731f76f5850
yujin song <uzini_@likelion.org>
2020-06-06 23:58:58 +0900

    [이슈해결-d18004c] post요청 에러 처리

--- blog-api/src/main/java/com/ion/blogapi/domain/Board.java
+++ blog-api/src/main/java/com/ion/blogapi/domain/Board.java
@@ -2,8 +2,10 @@ package com.ion.blogapi.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Board {
 	private Long id;
