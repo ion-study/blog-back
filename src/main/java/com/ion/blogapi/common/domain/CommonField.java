61477479bf76de1846f8f9719d0b0d1a7845ee7f
yujin song <uzini_@likelion.org>
2020-06-21 20:37:56 +0900

    공통 날짜 dateformat 변경

--- src/main/java/com/ion/blogapi/common/domain/CommonField.java
+++ src/main/java/com/ion/blogapi/common/domain/CommonField.java
@@ -2,7 +2,6 @@ package com.ion.blogapi.common.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.hibernate.search.annotations.*;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -11,12 +10,13 @@ import javax.persistence.PreUpdate;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by  : woonkicho
  * Created Day : 10/01/2020
- * Modified by  :
- * Modified Day :
+ * Modified by  :yujinsong
+ * Modified Day :21/06/2020
  */
 
 @MappedSuperclass
@@ -25,21 +25,22 @@ public abstract class CommonField implements Serializable {
 
 
 	private static final long serialVersionUID = -4808539823377841033L;
+	private static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-	@Column( name = "create_date", nullable = false, updatable = false)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_PATTERN, timezone = "Asia/Seoul")
+	@Column( nullable = false, updatable = false)
 	private Timestamp createdDate;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_PATTERN, timezone = "Asia/Seoul")
 	private Timestamp modifiedDate;
 
 
 	@PrePersist
 	protected void onCreate() {
-		createdDate = Timestamp.valueOf(LocalDateTime.now());
+		createdDate = Timestamp.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_PATTERN)));
 	}
 
 	@PreUpdate
 	protected void onUpdate() {
-		modifiedDate = Timestamp.valueOf(LocalDateTime.now());
+		modifiedDate = Timestamp.valueOf(LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_PATTERN)));
 	}
 }
